cc15851a34a1fd1d33fef95cf9626e088814635e
Proto-Layer <batterykol1337@gmail.com>
2025-08-15 15:17:04 +0300

    Create transactions.py

---
+++ Blockchain/structures/transactions.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict
+
+@dataclass
+class Transaction:
+    """Represents a single transaction in the blockchain."""
+    v: int
+    fee: str
+    creator: str
+    sig: str
+    tx_type: str
+    sig_type: str
+    nonce: int
+    payload: Dict[str, Any] = field(default_factory=dict)
